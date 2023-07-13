dc533c09beb99bec904142c7abdb357dbb41565f
guntza10 <jomphop.saibuatong@hotmail.com>
2023-07-14 00:04:58 +0700

    update user input js

---
+++ Javascript/03_user_input/index.js
@@ -0,0 +1,16 @@
+// How to accept user input
+
+// EASY WAY with a window prompt
+
+// let username = window.prompt("What's your name?");
+// console.log(username);
+
+// DIFFICULT WAY HTML textbox
+
+let username;
+
+document.getElementById("myButton").onclick = function () {
+  username = document.getElementById("myText").value;
+  console.log(username);
+  document.getElementById("myLabel").innerHTML = "Hello " + username;
+};
