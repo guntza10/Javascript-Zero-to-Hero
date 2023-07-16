9d24b5d16a19269c2be3a94683458ee3595eefbc
guntza10 <jomphop.saibuatong@hotmail.com>
2023-07-16 23:54:32 +0700

    update

--- Javascript/26_ternary_operator/index.js
+++ Javascript/26_ternary_operator/index.js
@@ -13,11 +13,9 @@ console.log(adult);
 function checkAge(age) {
   return age >= 18 ? true : false;
 }
-/*
+
 checkWinner(false);
- 
-function checkWinner(win){
- 
-    win ? console.log("You win!") : console.log("You lose!");
+
+function checkWinner(win) {
+  win ? console.log("You win!") : console.log("You lose!");
 }
-*/
