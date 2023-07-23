5a05bb6e64ce25fe042e09c3603f7b0138e8a551
guntza10 <jomphop.saibuatong@hotmail.com>
2023-07-23 17:36:25 +0700

    update js cookies

---
+++ Javascript/84_js_cookies/index.js
@@ -0,0 +1,35 @@
+const firstText = document.querySelector("#firstText");
+const lastText = document.querySelector("#lastText");
+const submitBtn = document.querySelector("#submitBtn");
+const cookieBtn = document.querySelector("#cookieBtn");
+
+submitBtn.addEventListener("click", () => {
+  setCookie("firstName", firstText.value, 365);
+  setCookie("lastName", lastText.value, 365);
+});
+cookieBtn.addEventListener("click", () => {
+  firstText.value = getCookie("firstName");
+  lastText.value = getCookie("lastName");
+});
+
+function setCookie(name, value, daysToLive) {
+  const date = new Date();
+  date.setTime(date.getTime() + daysToLive * 24 * 60 * 60 * 1000);
+  let expires = "expires=" + date.toUTCString();
+  document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+function deleteCookie(name) {
+  setCookie(name, null, null);
+}
+function getCookie(name) {
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArray = cDecoded.split("; ");
+  let result = null;
+
+  cArray.forEach((element) => {
+    if (element.indexOf(name) == 0) {
+      result = element.substring(name.length + 1);
+    }
+  });
+  return result;
+}
