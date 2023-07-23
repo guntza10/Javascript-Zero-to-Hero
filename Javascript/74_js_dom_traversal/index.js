17d80305fcd56e8e9d3e0940eda4687406702e04
guntza10 <jomphop.saibuatong@hotmail.com>
2023-07-23 17:14:03 +0700

    update dom traversal

---
+++ Javascript/74_js_dom_traversal/index.js
@@ -0,0 +1,11 @@
+// .firstElementChild
+// .lastElementChild
+// .parentElement
+// .nextElementSibling
+// .previousElementSibling
+// .children[]
+// Array.from(.children)
+
+let element = document.querySelector("#fruit");
+let child = element.firstElementChild;
+child.style.backgroundColor = "lightgreen";
