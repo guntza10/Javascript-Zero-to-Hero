701d899642a62581021677059d6998dc7b6fe4fe
guntza10 <jomphop.saibuatong@hotmail.com>
2023-07-23 16:40:31 +0700

    update js dom

--- Javascript/72_js_dom/index.js
+++ Javascript/72_js_dom/index.js
@@ -1,1 +1,12 @@
-// js dom
+// DOM = Document Object Model
+// represent html structure tree
+// โดยที่จะ ref ถึง tag ต่่างๆใน html เป็น hierarchy
+// เราสามารถใช้ javascript access dom เพื่อทำการเปลี่ยนแปลง structure,style,content ได้
+// document => คือการ access DOM ทั้งก้อน
+// console.dir(document) => list all property ของ document
+// document.title => access title ของ dom
+// document.URL => access URL ของ dom
+// document.location => access location url ของ dom
+// document.body.style.backgroundColor = "skyblue" => เปลี่ยน background body เป็นสี skyblue
+// document.getElementByID("key") => access element with id
+// document.getElementByID("key").innerHTML => access content element
