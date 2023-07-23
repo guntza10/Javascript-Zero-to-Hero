b9c8ea3908f70aa6f25d5b724970ec90924f2f93
guntza10 <jomphop.saibuatong@hotmail.com>
2023-07-23 17:19:47 +0700

    update add change css property

---
+++ Javascript/76_add_change_CSS_properties/index.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("myTitle");
+
+title.style.backgroundColor = "#222222";
+title.style.color = "rgb(50, 200, 250)";
+title.style.fontFamily = "consolas";
+title.style.textAlign = "center";
+title.style.border = "2px solid";
+title.style.display = "block";
