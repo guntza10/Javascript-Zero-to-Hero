bcae85ca4d03afe90fd937ff436059d17325c502
guntza10 <jomphop.saibuatong@hotmail.com>
2023-08-12 01:15:26 +0700

    update resolve code

--- Javascript/checkNumberIsOrder.js
+++ Javascript/checkNumberIsOrder.js
@@ -11,6 +11,26 @@ function solution(A, K) {
   if (A[0] != 1 && A[n - 1] != K) return false;
   else return true;
 }
+// solve: check ว่ามันเป็นลำดับมั้ย
+function solveSolution(A, K) {
+  var n = A.length;
+  for (var i = 0; i < n; i++) {
+    /*
+      วนลูปแต่ละตัวเช็คว่า +1 แล้วน้อยกว่าตัวต่อไปมั้ย
+      -> น้อยกว่าแสดงว่า มีลำดับที่หายไป
+      -> ถ้าเท่ากับ(ลำดับถัดไปที่ซ้ำกับตัวเอง) หรือ มากกว่า(ลำดับถัดไปที่ต่อจากตัวเอง) แสดงว่ามีลำดับต่อไป
+     */
+    if (A[i] + 1 < A[i + 1]) {
+      return false;
+    }
+  }
+  /*
+      - เช็คว่าตัวแรกไม่เป็น 1 ก็ผิด เพราะลำดับต้องเริ่มจาก 1
+      - เช็คว่าตัวสุดท้ายต้องเท่ากับ K เพราะ ลำดับมันคือ 1-K
+  */
+  if (A[0] != 1 || A[n - 1] != K) return false;
+  else return true;
+}
 
 const result = solution(A, K);
 console.log("result", result);
