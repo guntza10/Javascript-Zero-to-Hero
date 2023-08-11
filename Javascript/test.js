b75372f955eaf8bc115e20bae6a019c8050729de
guntza10 <jomphop.saibuatong@hotmail.com>
2023-08-11 21:50:02 +0700

    update

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,1 +1,17 @@
 // for test
+let A = [1, 2, 3];
+A = [-1, -3];
+function solution(A) {
+  const sortedNumberArr = A.sort((a, b) => a - b);
+  console.log("sortedNumberArr", sortedNumberArr);
+  let smallestMissingNumber = 1;
+  for (let i = 0; i < sortedNumberArr.length; i++) {
+    if (sortedNumberArr[i] === smallestMissingNumber) {
+      smallestMissingNumber++;
+    }
+  }
+  console.log("smallestMissingNumber", smallestMissingNumber);
+  return smallestMissingNumber;
+}
+
+solution(A);
